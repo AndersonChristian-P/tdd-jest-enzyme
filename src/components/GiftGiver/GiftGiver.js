85be18a6bdb1c555d5575326b0dd63e5259f0d13
Christian Anderson <ca96187@gmail.com>
2019-10-19 17:07:31 -0600

    setup GiftGiver component

---
+++ src/components/GiftGiver/GiftGiver.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+
+class GiftGiver extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Jello!
+      </div>
+    )
+  }
+
+}
+
+export default GiftGiver
